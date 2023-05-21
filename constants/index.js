935bb79339f117efa8d44bf8e24a03dbfd1c6a0a
Yarieos <vaughn.carmouche@protonmail.com>
2023-05-20 23:18:17 -0500

    New contract

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xb8cf1cB3475247F5713fc1d2A480C651846Db3a5";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-1-seven.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
